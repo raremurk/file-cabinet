591c20b629170224eeb350b8154f1e6aef21a357
Evgeny Fursevich <efursev@mail.ru>
2021-07-30 15:14:02 +0300

    Add FileCabinetGenerator.

---
+++ FileCabinetGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileCabinetGenerator
+{
+    /// <summary>Main class of the FileCabinetGenerator project.</summary>
+    public static class Program
+    {
+        /// <summary>Defines the entry point of the FileCabinetGenerator.</summary>
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
