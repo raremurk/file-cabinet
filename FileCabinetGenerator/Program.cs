3ad0f40acf020ae1c012eb2e0a2f001bd49eadda
Evgeny Fursevich <efursev@mail.ru>
2021-07-31 20:45:11 +0300

    Add command line parameters.

--- FileCabinetGenerator/Program.cs
+++ FileCabinetGenerator/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+[assembly: CLSCompliant(false)]
 
 namespace FileCabinetGenerator
 {
@@ -6,9 +10,110 @@ namespace FileCabinetGenerator
     public static class Program
     {
         /// <summary>Defines the entry point of the FileCabinetGenerator.</summary>
-        public static void Main()
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] availableFormats = { "csv", "xml" };
+            string[] fileExtensions = { ".csv", ".xml" };
+
+            string[] outputFormatArgs = { "--output-type=", "-t" };
+            string[] outputFileNameArgs = { "--output=", "-o" };
+            string[] amountOfRecordsArgs = { "--records-amount=", "-a" };
+            string[] startIdValueArgs = { "--start-id=", "-i" };
+
+            bool csvFormat = false;
+            bool xmlFormat = false;
+            string filename = null;
+            int amountOfRecords = 0;
+            int startIdValue = 0;
+
+            List<string> arguments = new (args);
+            arguments.Add(string.Empty);
+
+            for (int i = 0; i < arguments.Count - 1; i++)
+            {
+                if (!csvFormat && !xmlFormat)
+                {
+                    string format = ParseArgs(arguments[i], arguments[i + 1], outputFormatArgs);
+
+                    int index = Array.FindIndex(availableFormats, x => x.Equals(format, StringComparison.OrdinalIgnoreCase));
+                    csvFormat = index == 0;
+                    xmlFormat = index == 1;
+                }
+
+                if (string.IsNullOrEmpty(filename))
+                {
+                    string fileNameArg = ParseArgs(arguments[i], arguments[i + 1], outputFileNameArgs);
+
+                    if (fileNameArg.Length >= 5)
+                    {
+                        string fileExtension = fileNameArg[^4..];
+                        int index = Array.FindIndex(fileExtensions, i => i.Equals(fileExtension, StringComparison.OrdinalIgnoreCase));
+
+                        var way = fileNameArg.Split('\\', StringSplitOptions.RemoveEmptyEntries);
+                        string directory = way.Length > 1 ? string.Join('\\', way[0..^1]) : Directory.GetCurrentDirectory();
+
+                        if (new DirectoryInfo(directory).Exists)
+                        {
+                            filename = index == 0 && csvFormat ? fileNameArg : string.Empty;
+                            filename = index == 1 && xmlFormat ? fileNameArg : string.Empty;
+                        }
+                    }
+                }
+
+                if (amountOfRecords == 0)
+                {
+                    string amountOfRecordsArg = ParseArgs(arguments[i], arguments[i + 1], amountOfRecordsArgs);
+                    amountOfRecords = int.TryParse(amountOfRecordsArg, out int result) ? result : 0;
+                }
+
+                if (startIdValue == 0)
+                {
+                    string startIdValueArg = ParseArgs(arguments[i], arguments[i + 1], startIdValueArgs);
+                    startIdValue = int.TryParse(startIdValueArg, out int result) ? result : 0;
+                }
+            }
+
+            if (!csvFormat && !xmlFormat)
+            {
+                Console.WriteLine("Invalid format.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(filename))
+            {
+                Console.WriteLine("Invalid file name.");
+                return;
+            }
+
+            if (amountOfRecords == 0)
+            {
+                Console.WriteLine("Invalid amount of records.");
+                return;
+            }
+
+            if (startIdValue == 0)
+            {
+                Console.WriteLine("Invalid start Id.");
+                return;
+            }
+
+            Console.WriteLine($"{amountOfRecords} records were written to {filename}.");
+        }
+
+        private static string ParseArgs(string arg1, string arg2, string[] allowedArgs)
+        {
+            if (arg1.StartsWith(allowedArgs[0], StringComparison.OrdinalIgnoreCase))
+            {
+                return arg1[allowedArgs[0].Length..];
+            }
+
+            if (arg1.Equals(allowedArgs[1], StringComparison.OrdinalIgnoreCase))
+            {
+                return arg2;
+            }
+
+            return string.Empty;
         }
     }
 }
