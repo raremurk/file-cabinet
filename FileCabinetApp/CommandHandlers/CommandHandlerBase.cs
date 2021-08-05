0d3ffe8868f6c0be2448d868f675a0dea9564e1f
Evgeny Fursevich <efursev@mail.ru>
2021-08-06 00:21:12 +0300

    Chain of Responsibility template applied.

---
+++ FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FileCabinetApp.CommandHandlers
+{
+    /// <summary>Command handler base.</summary>
+    /// <seealso cref="ICommandHandler" />
+    public abstract class CommandHandlerBase : ICommandHandler
+    {
+        /// <summary>Gets the next handler.</summary>
+        /// <value>The next handler.</value>
+        public ICommandHandler NextHandler { get; private set; }
+
+        /// <summary>Handles the specified request.</summary>
+        /// <param name="request">The request.</param>
+        public abstract void Handle(AppCommandRequest request);
+
+        /// <summary>Sets the next handler.</summary>
+        /// <param name="handler">The handler.</param>
+        /// <exception cref="ArgumentNullException">Thrown when handler is null.</exception>
+        public void SetNext(ICommandHandler handler)
+        {
+            this.NextHandler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+    }
+}
